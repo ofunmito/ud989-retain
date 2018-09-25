8f79daa26a3dc135b5e097f2617a7bcd435d0e57
Funmito <funmito.odefemi@outlook.com>
2018-09-25 14:01:18 +0100

    doc: Add comments to code

--- js/retain.js
+++ js/retain.js
@@ -1,5 +1,7 @@
-$(function(){
+// app separates concerns
+$(function() {
 
+    // model
     var model = {
         init: function() {
             if (!localStorage.notes) {
@@ -16,9 +18,12 @@ $(function(){
         }
     };
 
-
+    // octopus: binds the view and the model together
     var octopus = {
         addNewNote: function(noteStr) {
+            // the addNewNote calls the model's add method 
+            // to add a New Note and then render the new note
+            // the view isn't directly modifying the model
             model.add({
                 content: noteStr
             });
@@ -35,27 +40,34 @@ $(function(){
         }
     };
 
-
+    // this is the view.
     var view = {
         init: function() {
+            // query the notes
             this.noteList = $('#notes');
+            // query the form
             var newNoteForm = $('#new-note-form');
+            // query the form's input
             var newNoteContent = $('#new-note-content');
-            newNoteForm.submit(function(e){
+            newNoteForm.submit(function(e) {
+                // get the content of the form input
                 octopus.addNewNote(newNoteContent.val());
                 newNoteContent.val('');
                 e.preventDefault();
             });
             view.render();
         },
-        render: function(){
+
+        // render the view using the getNOtes method on the octopus
+        render: function() {
             var htmlStr = '';
-            octopus.getNotes().forEach(function(note){
-                htmlStr += '<li class="note">'+
-                        note.content +
+            octopus.getNotes().forEach(function(note) {
+                // append new notes to the 'notes' unordered list
+                htmlStr += '<li class="note">' +
+                    note.content +
                     '</li>';
             });
-            this.noteList.html( htmlStr );
+            this.noteList.html(htmlStr);
         }
     };
 
